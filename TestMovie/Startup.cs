68abfa2b8518b30b673568f49c390c94dce13de1
MarkMIbrahim <mark.monir123@gmail.com>
2016-05-17 00:50:34 +0300

    mark nw

---
+++ TestMovie/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestMovie.Startup))]
+namespace TestMovie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
